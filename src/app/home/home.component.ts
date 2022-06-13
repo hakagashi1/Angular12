0cc63c53619c26dacaf21f20d17f10660541c1cd
thytienph <hakagashi1@gmail.com>
2022-06-13 15:33:39 +0700

    Done aps dujng

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,19 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   public name = 'Thủy Tiên';
   public age = 21;
 
   public traiCay = ['Táo', 'Nho', 'Cam', 'Quýt', 'Ổi'];
+  public traiCay2 = [
+    { ten: 'Táo', gia: 12, haGia: true },
+    { ten: 'Nho', gia: -3, haGia: false },
+    { ten: 'Cam', gia: 5, haGia: true },
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    console.log('trai cay = ', this.traiCay);
+    console.log('trai cay = ', this.traiCay2);
   }
-
 }

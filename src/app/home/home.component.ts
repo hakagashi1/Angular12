f8f161618cb69fad6aae0602bcf2292e98734f44
thytienph <hakagashi1@gmail.com>
2022-06-14 13:31:01 +0700

    update common services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../Services/common.service';
 
 @Component({
   selector: 'app-home',
@@ -63,11 +64,16 @@ export class HomeComponent implements OnInit {
       ],
     },
   ];
+  public counter = 0;
+  public counterBinhPhuong = 0;
 
-  constructor() {}
+  constructor(private common: CommonService) {}
 
   ngOnInit(): void {
     console.log('vietnamData = ', this.vietnamData);
+    this.counter = this.common.counter;
+    this.counterBinhPhuong = this.common.binhPhuong(this.counter);
+    this.common.counter++;
   }
 
   public changeCity(event: any): void {

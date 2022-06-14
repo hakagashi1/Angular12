5b98115a067d74c3c99c8eec06a2ad176e0aca27
thytienph <hakagashi1@gmail.com>
2022-06-14 10:05:56 +0700

    Update comboBox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,9 +16,75 @@ export class HomeComponent implements OnInit {
     { ten: 'Cam', gia: 5, haGia: true },
   ];
 
+  public districts: string[] = [];
+
+  public vietnamData = [
+    { city: 'Chọn thành phố', district: ['Quận / Huyện'] },
+    {
+      city: 'An Giang',
+      district: [
+        'Thành phố Long Xuyên',
+        'Thành phố Châu Đốc',
+        'Thị xã Tân Châu',
+        'Huyện An Phú',
+        'Huyện Châu Phú',
+        'Huyện Châu Thành',
+        'Huyện Chợ Mới',
+        'Huyện Phú Tân',
+        'Huyện Thoại Sơn',
+        'Huyện Tịnh Biên',
+        'Huyện Tri Tôn',
+      ],
+    },
+    {
+      city: 'Bà Rịa - Vũng Tàu',
+      district: [
+        'Thành phố Vũng Tàu',
+        'Thị xã Bà Rịa',
+        'Thị xã Phú Mỹ',
+        'Huyện Châu Đức',
+        'Huyện Côn Đảo',
+        'Huyện Đất Đỏ',
+        'Huyện Long Điền',
+        'Huyện Tân Thành',
+        'Huyện Xuyên Mộc',
+      ],
+    },
+    {
+      city: 'Bạc Liêu',
+      district: [
+        'Thành phố Bạc Liêu',
+        'Huyện Đông Hải',
+        'Huyện Giá Rai',
+        'Huyện Hòa Bình',
+        'Huyện Hồng Dân',
+        'Huyện Phước Long',
+        'Huyện Vĩnh Lợi',
+      ],
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {
-    console.log('trai cay = ', this.traiCay2);
+    console.log('vietnamData = ', this.vietnamData);
+  }
+
+  public changeCity(event: any): void {
+    const city = event.target.value;
+    if (!city) {
+      return;
+    }
+
+    // cách 1
+    // const search = this.vietnamData.filter(data => data.city === city);
+    // console.log('search', search);
+    // if (search && search.length > 0) {
+    //   this.districts = search[0].district;
+    // }
+
+    //cách 2
+    this.districts =
+      this.vietnamData.find((data) => data.city === city)?.district || [];
   }
 }

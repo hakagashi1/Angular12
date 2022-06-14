ab2c3e941686f15395fbcd072609e00a87d091c1
thytienph <hakagashi1@gmail.com>
2022-06-14 14:17:46 +0700

    Create new template driven form component

--- src/app/Services/common.service.ts
+++ src/app/Services/common.service.ts
@@ -12,4 +12,8 @@ export class CommonService {
   public binhPhuong(n: number) : number {
     return n * n;
   }
+
+  public submitData(data : any) : void {
+    console.log('Gui data len server. Data = ', data);
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'template-driven-form', component: TemplateDrivenFormComponent },
   { path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page
 ];
 
